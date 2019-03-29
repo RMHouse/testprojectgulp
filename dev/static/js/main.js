de51cce79fe6abdbd51e4c924622d8635a449747
Vitaly Trushin <webtrushin@gmail.com>
2019-03-29 22:51:12 +0300

    first commit

---
+++ dev/static/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    svg4everybody({});
+
+    var sandwich = function () {
+        $(document).on('click', '.sandwich', function () {
+            $(this).toggleClass('sandwich--active');
+
+        });
+    };
+    sandwich();
+});
